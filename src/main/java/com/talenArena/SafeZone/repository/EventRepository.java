ca268e77b0030c5df909aa3899bb647c6c22c2f0
catalina <catalina.genovese.7e8@itb.cat>
2026-03-02 11:40:12 +0100

    Remove unused import statements from README.md and EventRepository.java

--- src/main/java/com/talenArena/SafeZone/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.talenArena.SafeZone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
-
